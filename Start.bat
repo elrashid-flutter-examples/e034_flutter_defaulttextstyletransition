271ed5e25d4af8518a9b7b7827ee326f02b2a934
moh <osman.elrashid@gmail.com>
2019-08-07 12:15:10 +0400

    initial files

---
+++ Start.bat
@@ -0,0 +1,5 @@
+call env.bat 
+cd e034_flutter_defaulttextstyletransition 
+start ConEmu.exe -runlist cmd /K run.emulator.bat ^|^|^| cmd /K ping 127.0.0.1 -n 6 ^> nul ^& run.app.bat 
+call Code .>nul
+Exit 
